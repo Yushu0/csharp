ce1630efb14cbdd6df2b796a38c3b193c088c6c0
Yushu0 <119829083+Yushu0@users.noreply.github.com>
2023-04-14 22:22:47 -0600

    Add files via upload

---
+++ Assignment3/Assignment-Skeleton/utility/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Skeleton.utility
+{
+    public class Node
+    {
+        private static object _node = null;
+        public static object _Node
+        {
+            get { return _node; }
+            set { _node = value; }
+        }
+
+        public static object GetNode(LinkedListADT sLL, int index)
+        {
+            return sLL.Retrieve(index);
+        }
+
+        public static void SetNode(LinkedListADT sLL, object obj, int index)
+        {
+            sLL.Insert(obj, index);
+        }
+    }
+}
